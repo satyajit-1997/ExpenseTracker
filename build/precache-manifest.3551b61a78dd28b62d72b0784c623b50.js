f3073fce8179ec3ded3379488d3b00c31c5864fa
satyajit pradhan <satyajit1997p@gmail.com>
2022-12-08 00:02:54 +0530

    hosting

---
+++ build/precache-manifest.3551b61a78dd28b62d72b0784c623b50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5226b23dd1b62030cc4ebe41f45584",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "c5b735dbd0387875b461",
+    "url": "/ExpenseTracker/static/css/main.bb80dfda.chunk.css"
+  },
+  {
+    "revision": "bd6bd55d0f17c3088198",
+    "url": "/ExpenseTracker/static/js/2.b190f742.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/ExpenseTracker/static/js/2.b190f742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b735dbd0387875b461",
+    "url": "/ExpenseTracker/static/js/main.38ea0b71.chunk.js"
+  },
+  {
+    "revision": "e25630aad13bae8f9aae",
+    "url": "/ExpenseTracker/static/js/runtime-main.0e810c6f.js"
+  }
+]);
